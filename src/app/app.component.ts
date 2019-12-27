32ceec4ce9ff1c6a05a4ada1e72367cf840af21a
Siddhartha Shrestha <syd.earth@gmail.com>
2019-12-27 11:42:51 +0545

    Angular commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-app';
+  servername = 'Ashok';
+
+  toggelstatus()
+  {
+ 
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { RedComponent } from './red/red.component';
+import { GreenComponent } from './green/green.component';
+import { YellowComponent } from './yellow/yellow.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    RedComponent,
+    GreenComponent,
+    YellowComponent
   ],
   imports: [
     BrowserModule
